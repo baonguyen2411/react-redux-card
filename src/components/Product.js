291698a7d2eabd4ac8f2ae1ee45b5b5f547ffa45
BaoNG <baong@fsoft.com.vn>
2019-04-15 22:03:38 +0700

    add function showRating

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Product extends Component {
+  showRating(rating) {
+    let result = [];
+    for (var i = 1; i <= rating; i++) {
+      result.push(<i className="fa fa-star"/>);
+    }
+    for (var j = 1; j <= 5 - rating; j++) {
+      result.push(<i className="fa fa-star-o"/>);
+    }
+    return result;
+  }
+
   render() {
     var { product } = this.props;
 
@@ -9,11 +20,7 @@ class Product extends Component {
       <div className="col-lg-4 col-md-6 mb-r">
         <div className="card text-center card-cascade narrower">
           <div className="view overlay hm-white-slight z-depth-1">
-            <img
-              src={ product.image }
-              className="img-fluid"
-              alt=""
-            />
+            <img src={product.image} className="img-fluid" alt="" />
             <a href="!">
               <div className="mask waves-light waves-effect waves-light" />
             </a>
@@ -21,29 +28,15 @@ class Product extends Component {
           <div className="card-body">
             <h4 className="card-title">
               <strong>
-                <a href="!">{ product.name }</a>
+                <a href="!">{product.name}</a>
               </strong>
             </h4>
             <ul className="rating">
-              <li>
-                <i className="fa fa-star" />
-              </li>
-              <li>
-                <i className="fa fa-star" />
-              </li>
-              <li>
-                <i className="fa fa-star" />
-              </li>
-              <li>
-                <i className="fa fa-star" />
-              </li>
-              <li>
-                <i className="fa fa-star" />
-              </li>
+              <li>{this.showRating(product.rating)}</li>
             </ul>
-            <p className="card-text">{ product.description }</p>
+            <p className="card-text">{product.description}</p>
             <div className="card-footer">
-              <span className="left">{ product.price }$</span>
+              <span className="left">{product.price}$</span>
               <span className="right">
                 <a
                   href="!"
